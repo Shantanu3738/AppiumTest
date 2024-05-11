8d8bc1c2babdd1efbf84c044e2b67b3d573ee310
shaan <shantanudey009@gmail.com>
2024-05-11 13:36:19 +0530

    Created Adnroid Driver

--- src/test/java/setup/setupAndroidTest.java
+++ src/test/java/setup/setupAndroidTest.java
@@ -1,9 +1,22 @@
 package setup;
 
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.options.UiAutomator2Options;
+import io.appium.java_client.service.local.AppiumDriverLocalService;
+import io.appium.java_client.service.local.AppiumServiceBuilder;
+
 public class setupAndroidTest {
 
     public void setupDriver(String appPath)
     {
+        AppiumDriverLocalService service = new AppiumServiceBuilder().usingAnyFreePort().build();
+        service.start();
+        UiAutomator2Options options = new UiAutomator2Options();
+        options.setApp(appPath);
+        AndroidDriver driver = new AndroidDriver(service.getUrl(), options);
+    }
 
+    public static void main(String[] args) {
+        new setupAndroidTest().setupDriver("D:\\Study\\MaterialLoginExample.apk");
     }
 }
